49a3bd6359b026dd8a350dfd3320d68019e96031
Benjamin Seber <benjamin.seber@focus-shift.de>
2020-11-22 18:13:40 +0100

    move default meta values into data json file

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,18 @@
+const metadata = require("./src/_data/metadata.json");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "./src/static": "." });
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/js");
+
+  eleventyConfig.addFilter("title", (title) =>
+    title ? metadata.title + " " + title : metadata.title,
+  );
+
+  eleventyConfig.addFilter(
+    "description",
+    (description) => description || metadata.description,
+  );
 
   return {
     dir: {
