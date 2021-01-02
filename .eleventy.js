dc9ebedeba942427af47d2677f148f26db8c7969
Benjamin Seber <benjamin.seber@focus-shift.de>
2021-01-02 12:07:03 +0100

    use current year in footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,14 @@
+const formatDate = require("date-fns/format");
 const metadata = require("./src/_data/metadata.json");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("title", (title) =>
     title ? metadata.title + " - " + title : metadata.title,
   );
+
+  eleventyConfig.addFilter("date", function (date, format) {
+    return formatDate(date, format);
+  });
 
   eleventyConfig.addFilter(
     "description",
